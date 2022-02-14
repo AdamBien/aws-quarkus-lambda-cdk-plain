ac18d4f80ef4a3b87d17d3f3711d4eef7938794e
Adam Bien <abien@adam-bien.com>
2022-02-14 15:34:50 +0100

    MediaType fixed

--- lambda-st/src/test/java/airhacks/lambda/greetings/boundary/GreetingsResource.java
+++ lambda-st/src/test/java/airhacks/lambda/greetings/boundary/GreetingsResource.java
@@ -1,7 +1,9 @@
 package airhacks.lambda.greetings.boundary;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -9,6 +11,7 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 public interface GreetingsResource {
 
     @GET
+    @Consumes(MediaType.TEXT_PLAIN)
     @Path("hello")
     String content();
 
