fff6e5b1d66a8166bb15583230881dee76671450
Adam Bien <abien@adam-bien.com>
2022-12-14 14:10:04 +0100

    SnapStart is activated

--- cdk/src/main/java/airhacks/lambda/control/QuarkusLambda.java
+++ cdk/src/main/java/airhacks/lambda/control/QuarkusLambda.java
@@ -4,28 +4,49 @@ import java.util.Map;
 
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.services.lambda.Architecture;
+import software.amazon.awscdk.services.lambda.CfnFunction;
 import software.amazon.awscdk.services.lambda.Code;
-import software.amazon.awscdk.services.lambda.IFunction;
 import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.IFunction;
 import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.Version;
 import software.constructs.Construct;
 
-public class QuarkusLambda extends Construct{
+public class QuarkusLambda extends Construct {
 
     static Map<String, String> configuration = Map.of(
             "message", "hello, quarkus as AWS Lambda",
             "JAVA_TOOL_OPTIONS", "-XX:+TieredCompilation -XX:TieredStopAtLevel=1");
     static String lambdaHandler = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest";
-    static int memory = 1024; //~0.5 vCPU
+    static int memory = 1024; // ~0.5 vCPU
     static int timeout = 10;
     IFunction function;
 
-    public QuarkusLambda(Construct scope,String functionName) {
-        super(scope, "QuarkusLambda");
-        this.function = createFunction(functionName, lambdaHandler, configuration, memory, timeout);
+    public QuarkusLambda(Construct scope, String functionName){
+        this(scope,functionName,true);
     }
 
-    IFunction createFunction(String functionName,String functionHandler, Map<String,String> configuration, int memory, int timeout) {
+    public QuarkusLambda(Construct scope, String functionName, boolean snapStart) {
+        super(scope, "QuarkusLambda");
+        this.function = createFunction(functionName, lambdaHandler, configuration, memory, timeout);
+        if (snapStart)
+            this.function = setupSnapStart(this.function);
+    }
+
+    Version setupSnapStart(IFunction function) {
+        var defaultChild = this.function.getNode().getDefaultChild();
+        if (defaultChild instanceof CfnFunction) {
+            var cfnFunction = (CfnFunction) defaultChild;
+            cfnFunction.addPropertyOverride("SnapStart", Map.of("ApplyOn", "PublishedVersions"));
+        }
+        return Version.Builder.create(this, "SnapStartVersion")
+                .lambda(this.function)
+                .description("SnapStart")
+                .build();
+    }
+
+    IFunction createFunction(String functionName, String functionHandler, Map<String, String> configuration, int memory,
+            int timeout) {
         return Function.Builder.create(this, functionName)
                 .runtime(Runtime.JAVA_11)
                 .architecture(Architecture.ARM_64)
@@ -36,9 +57,9 @@ public class QuarkusLambda extends Construct{
                 .environment(configuration)
                 .timeout(Duration.seconds(timeout))
                 .build();
-    }    
+    }
 
-    public IFunction getFunction(){
+    public IFunction getFunction() {
         return this.function;
     }
 }
