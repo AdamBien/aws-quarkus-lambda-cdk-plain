c7fd71461d1926cd04f798628beade790df2b850
Adam Bien <abien@adam-bien.com>
2023-10-19 05:40:29 +0200

    migration to Jakarta EE / dependencies updated

--- lambda-st/src/test/java/airhacks/lambda/greetings/boundary/GreetingsResource.java
+++ lambda-st/src/test/java/airhacks/lambda/greetings/boundary/GreetingsResource.java
@@ -1,11 +1,11 @@
 package airhacks.lambda.greetings.boundary;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @RegisterRestClient(configKey = "base_uri")
 public interface GreetingsResource {
--- lambda-st/src/test/java/airhacks/lambda/greetings/boundary/GreetingsResourceIT.java
+++ lambda-st/src/test/java/airhacks/lambda/greetings/boundary/GreetingsResourceIT.java
@@ -1,14 +1,12 @@
 package airhacks.lambda.greetings.boundary;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import javax.inject.Inject;
-
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
 
 @QuarkusTest
 public class GreetingsResourceIT {
