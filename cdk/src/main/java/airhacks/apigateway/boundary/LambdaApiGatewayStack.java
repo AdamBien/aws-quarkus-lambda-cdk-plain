2f449404d4f499b76587bd4026498550fafbdcd8
Adam Bien <abien@adam-bien.com>
2023-11-21 18:39:32 +0100

    dedicated http api builder added

--- cdk/src/main/java/airhacks/apigateway/boundary/LambdaApiGatewayStack.java
+++ cdk/src/main/java/airhacks/apigateway/boundary/LambdaApiGatewayStack.java
@@ -4,18 +4,49 @@ import airhacks.InfrastructureBuilder;
 import airhacks.apigateway.control.APIGatewayIntegrations;
 import airhacks.lambda.control.QuarkusLambda;
 import software.amazon.awscdk.Stack;
+import software.constructs.Construct;
 
 public class LambdaApiGatewayStack extends Stack {
 
-    //true -> integrates with Http API, false -> integrates with REST API
-    static boolean HTTP_API_GATEWAY_INTEGRATION = true;
     static String FUNCTION_NAME  = "airhacks_lambda_gretings_boundary_Greetings";
 
-    public LambdaApiGatewayStack(InfrastructureBuilder builder) {
-        super(builder.construct(), builder.stackId());    
+    public static class LambdaApiGatewayBuilder{
+        private InfrastructureBuilder builder;
+        private boolean httpApiGateway;
+        
+        LambdaApiGatewayBuilder(InfrastructureBuilder builder){
+            this.builder = builder;
+        }
 
-        var quarkuLambda = new QuarkusLambda(this,FUNCTION_NAME,builder.configuration());
-        new APIGatewayIntegrations(this, HTTP_API_GATEWAY_INTEGRATION, quarkuLambda.getFunction());
+        LambdaApiGatewayBuilder withRestAPI(){
+            this.httpApiGateway = false;
+            return this;
+        }
+
+
+        InfrastructureBuilder infrastructureBuilder(){
+            return this.builder;
+        }
+
+        public Construct construct(){
+            return this.builder.construct();
+        }
+
+        public String stackId(){
+            return this.builder.stackId();
+        }
+
+        public LambdaApiGatewayStack build(){
+            return new LambdaApiGatewayStack(this);
+        }
+
+    }
+
+    public LambdaApiGatewayStack(LambdaApiGatewayBuilder builder) {
+        super(builder.construct(), builder.stackId());    
+        var infrastructureBuilder = builder.infrastructureBuilder();
+        var quarkuLambda = new QuarkusLambda(this,infrastructureBuilder.functionName(),infrastructureBuilder.configuration());
+        new APIGatewayIntegrations(this, builder.httpApiGateway, quarkuLambda.getFunction());
     }
 
 
