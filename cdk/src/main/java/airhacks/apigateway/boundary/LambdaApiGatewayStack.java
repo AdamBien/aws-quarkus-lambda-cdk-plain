45f0d6f4b906dadbe2c0550a39725a5e4f2ae5b4
Adam Bien <abien@adam-bien.com>
2023-11-27 05:41:49 +0100

    private visibility assumes REST api

--- cdk/src/main/java/airhacks/apigateway/boundary/LambdaApiGatewayStack.java
+++ cdk/src/main/java/airhacks/apigateway/boundary/LambdaApiGatewayStack.java
@@ -4,77 +4,70 @@ import airhacks.InfrastructureBuilder;
 import airhacks.apigateway.control.APIGatewayIntegrations;
 import airhacks.lambda.control.QuarkusLambda;
 import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.services.ec2.IVpc;
 import software.constructs.Construct;
 
 public class LambdaApiGatewayStack extends Stack {
 
-
-    public static class LambdaApiGatewayBuilder{
+    public static class LambdaApiGatewayBuilder {
         private InfrastructureBuilder builder;
         private boolean httpApiGateway;
         private boolean privateVPCAccessibility;
         private String vpcId;
-        
-        public LambdaApiGatewayBuilder(InfrastructureBuilder builder){
+
+        public LambdaApiGatewayBuilder(InfrastructureBuilder builder) {
             this.builder = builder;
         }
 
-        public LambdaApiGatewayBuilder withRestAPI(){
+        public LambdaApiGatewayBuilder withRestAPI() {
             this.httpApiGateway = false;
             return this;
         }
 
-        public LambdaApiGatewayBuilder withPrivateVPCAccessibility(){
-            this.privateVPCAccessibility = true;
-            return this;
+        public LambdaApiGatewayBuilder withPrivateVPCAccessibility() {
+            return withPrivateVPCAccessibility(null);
         }
 
-        public LambdaApiGatewayBuilder withPrivateVPCAccessibility(String vpcId){
+        public LambdaApiGatewayBuilder withPrivateVPCAccessibility(String vpcId) {
             this.vpcId = vpcId;
             this.privateVPCAccessibility = true;
             return this;
         }
 
-
-        InfrastructureBuilder infrastructureBuilder(){
+        InfrastructureBuilder infrastructureBuilder() {
             return this.builder;
         }
 
-        public boolean isHttpApiGateway(){
+        public boolean isHttpApiGateway() {
             return this.httpApiGateway;
         }
 
-
-
-        public boolean isPrivateVPCVisibility(){
+        public boolean isPrivateVPCVisibility() {
             return this.privateVPCAccessibility;
         }
 
-
-        public Construct construct(){
+        public Construct construct() {
             return this.builder.construct();
         }
 
-        public String vpcId(){
+        public String vpcId() {
             return this.vpcId;
         }
 
-
-        public String stackId(){
+        public String stackId() {
             return this.builder.stackId();
         }
 
-        public LambdaApiGatewayStack build(){
+        public LambdaApiGatewayStack build() {
             return new LambdaApiGatewayStack(this);
         }
 
     }
 
     public LambdaApiGatewayStack(LambdaApiGatewayBuilder builder) {
-        super(builder.construct(), builder.stackId());    
+        super(builder.construct(), builder.stackId());
         var infrastructureBuilder = builder.infrastructureBuilder();
-        var quarkuLambda = new QuarkusLambda(this,infrastructureBuilder.functionName(),infrastructureBuilder.configuration());
+        var quarkuLambda = new QuarkusLambda(this, infrastructureBuilder.functionName(),
+                infrastructureBuilder.configuration());
         new APIGatewayIntegrations(this, builder, quarkuLambda.getFunction());
     }
 }
