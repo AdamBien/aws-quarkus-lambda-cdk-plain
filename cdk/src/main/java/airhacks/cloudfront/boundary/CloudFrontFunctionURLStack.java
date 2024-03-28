d539bda42e57ca18bdea7fbf1cf053c5469a0505
Adam Bien <abien@adam-bien.com>
2024-03-28 07:40:16 +0100

    cloudfront distribution builder integrated

--- cdk/src/main/java/airhacks/cloudfront/boundary/CloudFrontFunctionURLStack.java
+++ cdk/src/main/java/airhacks/cloudfront/boundary/CloudFrontFunctionURLStack.java
@@ -1,7 +1,6 @@
 package airhacks.cloudfront.boundary;
 
 import airhacks.InfrastructureBuilder;
-import airhacks.functionurl.boundary.FunctionURLStack;
 import airhacks.lambda.control.QuarkusLambda;
 import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Stack;
@@ -9,11 +8,9 @@ import software.amazon.awscdk.services.cloudfront.AllowedMethods;
 import software.amazon.awscdk.services.cloudfront.BehaviorOptions;
 import software.amazon.awscdk.services.cloudfront.CachePolicy;
 import software.amazon.awscdk.services.cloudfront.Distribution;
-import software.amazon.awscdk.services.cloudfront.OriginProtocolPolicy;
-import software.amazon.awscdk.services.cloudfront.SSLMethod;
 import software.amazon.awscdk.services.cloudfront.SecurityPolicyProtocol;
 import software.amazon.awscdk.services.cloudfront.ViewerProtocolPolicy;
-import software.amazon.awscdk.services.cloudfront.origins.HttpOrigin;
+import software.amazon.awscdk.services.cloudfront.origins.FunctionUrlOrigin;
 import software.amazon.awscdk.services.lambda.FunctionUrlAuthType;
 import software.amazon.awscdk.services.lambda.FunctionUrlOptions;
 import software.constructs.Construct;
@@ -21,7 +18,6 @@ import software.constructs.Construct;
 public class CloudFrontFunctionURLStack extends Stack {
     public static class CloudFrontFunctionURLBuilder {
         private InfrastructureBuilder infrastructureBuilder;
-        private FunctionUrlAuthType authType = FunctionUrlAuthType.NONE;
 
         public CloudFrontFunctionURLBuilder(InfrastructureBuilder infrastructureBuilder) {
             this.infrastructureBuilder = infrastructureBuilder;
@@ -52,18 +48,20 @@ public class CloudFrontFunctionURLStack extends Stack {
                 infrastructureBuilder.configuration());
         var function = quarkusLambda.getFunction();
         var functionUrl = function.addFunctionUrl(FunctionUrlOptions.builder()
-                .authType(builder.authType)
+                .authType(FunctionUrlAuthType.NONE)
                 .build());
+        var functionURLOrigin = new FunctionUrlOrigin(functionUrl);
         var distribution = Distribution.Builder.create(this, "FunctionURLDistribution")
                 .minimumProtocolVersion(SecurityPolicyProtocol.SSL_V3)
                 .defaultBehavior(BehaviorOptions.builder()
-                        .origin(new HttpOrigin(functionUrl.getUrl()))
+                        .origin(functionURLOrigin)
                         .allowedMethods(AllowedMethods.ALLOW_ALL)
                         .viewerProtocolPolicy(ViewerProtocolPolicy.REDIRECT_TO_HTTPS)
                         .cachePolicy(CachePolicy.CACHING_DISABLED)
                         .build())
                 .build();
-        CfnOutput.Builder.create(this, "CloudFrontDistributionDomainNameOutput").value(distribution.getDistributionDomainName()).build();
+        CfnOutput.Builder.create(this, "CloudFrontDistributionDomainNameOutput")
+                .value(distribution.getDistributionDomainName()).build();
 
     }
 
